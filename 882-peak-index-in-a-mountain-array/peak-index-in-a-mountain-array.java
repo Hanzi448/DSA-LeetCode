79aaceab43103501ba5fa03646b3d24398d924ac
hitms-demo <hitmsannonymous@gmail.com>
2026-01-05 17:57:53 +0500

    Time: 0 ms (100.00%) | Memory: 80.1 MB (76.09%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
@@ -1,11 +1,16 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        int peek = 0;
-        for(int i = 1; i < arr.length; i++){
-            if(arr[i] > arr[peek]){
-                peek = i;
+        int low = 0;
+        int high = arr.length - 1;
+        while(low < high){
+            int mid = (low + high) / 2;
+            if(arr[mid] < arr[mid +1]){
+                low = mid + 1;
+            }
+            else {
+                high = mid;
             }
         }
-        return peek;
+        return low;
     }
-}
+}    
